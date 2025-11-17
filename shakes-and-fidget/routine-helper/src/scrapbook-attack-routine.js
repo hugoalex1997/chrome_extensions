83e852a36d4fbd00cc456aca584c6f274fbe4ee7
hugo.gomes <hugo.gomes@fabamaq.com>
2025-11-17 14:08:55 +0000

    chore: remove debug log

--- shakes-and-fidget/routine-helper/src/scrapbook-attack-routine.js
+++ shakes-and-fidget/routine-helper/src/scrapbook-attack-routine.js
@@ -88,7 +88,7 @@ export class ScrapBookAttackRoutine {
       console.warn('ScrapBookAttacksRoutine :: No player found at rank: ' + rank)
       return
     }
-    
+
     const equipmentInfo = await this.http.GetPlayerInformation(playerName)
 
     this.equipmentInspector.UpdateUnownedItems(this.scrapbook, playerName, equipmentInfo)
